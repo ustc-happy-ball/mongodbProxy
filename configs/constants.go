5519b929c0f7b6b6ba220acfb9bf64f59e453345
song <s1322023401@gmail.com>
2021-03-30 15:26:23 +0800

    [init] 初始化项目

---
+++ configs/constants.go
@@ -0,0 +1,8 @@
+package configs
+
+const (
+	// mongodb
+	MongoUri string = "mongodb://localhost:27017"
+	MongoPoolSize uint64 = 100
+	MongoDatabase string = "happyball"
+)
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/TianqiS/database_for_happyball
+
+go 1.15
+
+require go.mongodb.org/mongo-driver v1.5.0 // indirect
