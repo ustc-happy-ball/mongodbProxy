9800bf3b861a9ead32229a4a2fe110762e5c2373
song <s1322023401@gmail.com>
2021-04-13 20:41:20 +0800

    [delete] 添加delete request decoder

--- internal/event/request/delete_item_by_id.go
+++ internal/event/request/delete_item_by_id.go
@@ -1,8 +1,13 @@
 package request
 
-import "github.com/TianqiS/database_for_happyball/framework"
+import (
+	databaseGrpc "github.com/TianqiS/database_for_happyball/database_grpc"
+	"github.com/TianqiS/database_for_happyball/framework"
+)
 
 type DeleteItemById struct {
+	deleteItem int32
+	itemId string
 	*framework.BaseEvent
 }
 
@@ -10,8 +15,10 @@ func (deleteItemById *DeleteItemById) ToMessage() interface{} {
 	return nil
 }
 
-func (deleteItemById *DeleteItemById) FromMessage(interface{}) {
-
+func (deleteItemById *DeleteItemById) FromMessage(message interface{}) {
+	messagePb := message.(*databaseGrpc.DeleteItemById)
+	deleteItemById.deleteItem = int32(messagePb.GetDeleteItem())
+	deleteItemById.itemId = messagePb.GetItemId()
 }
 
 
--- internal/event/request/delete_item_by_key.go
+++ internal/event/request/delete_item_by_key.go
@@ -1,8 +1,13 @@
 package request
 
-import "github.com/TianqiS/database_for_happyball/framework"
+import (
+	databaseGrpc "github.com/TianqiS/database_for_happyball/database_grpc"
+	"github.com/TianqiS/database_for_happyball/framework"
+)
 
 type DeleteItemByKey struct {
+	deleteItem int32
+	matchItem []*MatchItem
 	*framework.BaseEvent
 }
 
@@ -10,6 +15,14 @@ func (deleteItemByKey *DeleteItemByKey) ToMessage() interface{} {
 	return nil
 }
 
-func (deleteItemByKey *DeleteItemByKey) FromMessage(interface{}) {
-
+func (deleteItemByKey *DeleteItemByKey) FromMessage(message interface{}) {
+	messagePb := message.(*databaseGrpc.DeleteItemByKey)
+	deleteItemByKey.deleteItem = int32(messagePb.GetDeleteItem())
+	matchItemArr := messagePb.GetMatchItem()
+	deleteItemByKey.matchItem = make([]*MatchItem, len(matchItemArr))
+	for i, itemPb := range matchItemArr {
+		item := &MatchItem{}
+		item.FromMessage(itemPb)
+		deleteItemByKey.matchItem[i] = item
+	}
 }
