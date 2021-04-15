a32c8ba28fd994d3631d2859b0d194a7f94eb18e
song <s1322023401@gmail.com>
2021-04-15 21:06:03 +0800

    [add] 完善deleteById请求处理

--- db/client_test.go
+++ db/client_test.go
@@ -76,3 +76,31 @@ func TestClientFindByKey(t *testing.T) {
 	}
 	log.Printf("Greeting: %s", r.GetResponse())
 }
+
+func TestClientDeleteById(t *testing.T) {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := databaseGrpc.NewDbServiceClient(conn)
+
+	// Contact the server and print out its response.
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	requestMsg := &databaseGrpc.DbMessage{Id: 1,
+		MessageCode: databaseGrpc.MESSAGE_CODE_DELETE_BY_ID_REQUEST,
+		MessageType: databaseGrpc.MESSAGE_TYPE_REQUEST,
+		Request: &databaseGrpc.Request{
+		DeleteItemById: &databaseGrpc.DeleteItemById{
+			DeleteItem: databaseGrpc.ITEM_PLAYER,
+			ItemId:     "606fa5d26ce9e63ec70ac6d6",
+		},
+		}}
+	r, err := c.SendRequest(ctx, requestMsg)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetResponse())
+}
--- internal/handler/request_handler_delete_by_id.go
+++ internal/handler/request_handler_delete_by_id.go
@@ -1,6 +1,11 @@
 package handler
 
-import "github.com/TianqiS/database_for_happyball/framework/event"
+import (
+	"github.com/TianqiS/database_for_happyball/configs"
+	"github.com/TianqiS/database_for_happyball/db"
+	"github.com/TianqiS/database_for_happyball/framework/event"
+	event2 "github.com/TianqiS/database_for_happyball/internal/event"
+)
 
 type DeleteByIdRequestHandler struct {}
 
@@ -10,4 +15,14 @@ func (handler *DeleteByIdRequestHandler) OnEvent(e event.Event) {
 	if nil == e {
 		return
 	}
+	dbMessageEvent := e.(*event2.DbMessage)
+	deleteByIdRequest := dbMessageEvent.Request.DeleteItemById
+	itemType := deleteByIdRequest.DeleteItem
+	collection := db.CollectionMap[itemType]
+	err := collection.DeleteItemById(deleteByIdRequest.ItemId)
+	if err != nil {
+		dbMessageEvent.Error <- err
+	}
+	deleteResponse := event2.NewDeleteResMsg(configs.ResponseStatusOk, "")
+	dbMessageEvent.ResResult <- deleteResponse
 }
