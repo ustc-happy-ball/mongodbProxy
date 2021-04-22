7020efbfa5463589073bcec2ac6c3571fbe8d7b2
imilano <m15752836560_1@163.com>
2021-04-23 00:21:30 +0800

    Mofify mongoURI to allow access in cluster

--- configs/constants.go
+++ configs/constants.go
@@ -2,7 +2,7 @@ package configs
 
 const (
 	// mongodb
-	MongoUri string = "mongodb://localhost:27017"
+	MongoUri string = "mongodb://mongodb:27017"
 	MongoPoolSize uint64 = 100
 	MongoDatabase string = "happyball"
 
